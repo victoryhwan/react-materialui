ab896631510f1e2d9e2fa6ffb2d694d513737222
kihwan.lee <kihwan.lee@lemonhc.com>
2021-05-20 16:59:33 +0900

    마크다운 수정

--- src/component/CodeBlock.js
+++ src/component/CodeBlock.js
@@ -1,14 +1,31 @@
 import React from "react"
+// import Prism from "prismjs"
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
 import { tomorrow } from "react-syntax-highlighter/dist/esm/styles/prism"
 
-const CodeBlock = ({ language, value }) => {
-    console.log(`SyntaxHighlighter, ${value}`)
+const CodeBlock = ({ language, children }) => {
+    console.log(`SyntaxHighlighter, language:${language}, children:${children}`)
   return (
     <SyntaxHighlighter language={'javascript'} style={tomorrow}>
-      {value}
+      {children}
     </SyntaxHighlighter>
   )
 }
+
+// const CodeBlock = ({ language, value }) => {
+// 	// 1. no language was typed
+// 	// or 2. language doesnt exist
+// 	// if (!language || !Prism.languages[language] || !value)
+// 	// 	return <BlankCodeBlock value={value} />;
+
+// 	var html = Prism.highlight(value, Prism.languages[language]);
+// 	var cls = 'language-' + language;
+
+// 	return (
+// 		<pre className={cls}>
+// 			<code dangerouslySetInnerHTML={{ __html: html }} className={cls} />
+// 		</pre>
+// 	);
+// }
 
 export default CodeBlock
--- src/view/MarkDown.js
+++ src/view/MarkDown.js
@@ -1,29 +1,50 @@
 // About.js
 import React from 'react';
-
+import makeStyles from '@material-ui/core/styles/makeStyles';
 import ReactMarkdown from 'react-markdown';
 import CodeBlock from '../component/CodeBlock'
+import Paper from '@material-ui/core/Paper';
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      margin: theme.spacing(1,1),
+      padding: theme.spacing(1,1),
+      maxWidth: 1000,
+      minWidth: 'auto',
+      alignContent: 'center'
+    //   textAlign: 'center',
+      // color: theme.palette.text.secondary,
+    }
+  }));
 
 const markdown = `
 # 헤딩
+## 헤딩
+### 헤딩
+#### 헤딩
+##### 헤딩
+###### 헤딩
+
 **굵게**
 
 일반 텍스트
 
 \`\`\`
-    코드블럭
+    코드블럭'
 \`\`\`
 
 *기울이기*
 
-글자 \`배경색\`
+글자 \`\`배경색\`\`
 
 > 인용문
 `;
 
 const MarkDown = () => {
+    const classes = useStyles();
     return (
-        <ReactMarkdown renderers={{ code : CodeBlock}}>{markdown}</ReactMarkdown>
+        <Paper className={classes.paper}>
+            <ReactMarkdown components={{ code : CodeBlock}} children={markdown}/>
+        </Paper>
     );
 };
 
